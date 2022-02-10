214479f6fd09a6af79542c07483459331e836958
JollyReap <84176639+JollyReap@users.noreply.github.com>
2022-02-10 14:47:28 +0300

    .

--- main.py
+++
@@ -1,5 +0,0 @@
-# Welcome to logic game Bagels, in this game you will guess secret number from 3 figures.
-
-# Rules of this games:
-# 1. If you guess a right figure, then programm will print: Pico
-#
