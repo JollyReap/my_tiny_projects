d002f8e7fb4e6138b9fa3679b22fd5420e4b9851
v1ntex <leonidt668@gmail.com>
2022-02-10 14:46:10 +0300

    Game wokrs

---
+++ main.py
@@ -0,0 +1,5 @@
+# Welcome to logic game Bagels, in this game you will guess secret number from 3 figures.
+
+# Rules of this games:
+# 1. If you guess a right figure, then programm will print: Pico
+#
